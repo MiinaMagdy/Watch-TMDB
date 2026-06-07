c4ed7d9d7e1c56ad12a930eaf42ffc78392d83bd
Mina Magdy <mina.magdy.takawey@gmail.com>
2026-06-07 18:21:53 +0300

    test(movie) add unit tests for movies controller

--- src/movie/movie.controller.spec.ts
+++ src/movie/movie.controller.spec.ts
@@ -32,6 +32,45 @@ describe('MovieController', () => {
     expect(controller).toBeDefined();
   });
 
+  it('should find all movies when movies are found', async () => {
+    const movies = [{ id: 1 }, { id: 2 }];
+    mockMovieService.findAll.mockResolvedValue({ movies, total: 2, page: 1, limit: 10 });
+
+    const result = await controller.findAll({});
+
+    expect(result).toEqual({ movies, total: 2, page: 1, limit: 10 });
+    expect(mockMovieService.findAll).toHaveBeenCalledTimes(1);
+  })
+
+  it('should find one movie when movie is found', async () => {
+    const movie = { id: 1 };
+    mockMovieService.findOne.mockResolvedValue(movie);
+
+    const result = await controller.findOne(1);
+
+    expect(result).toEqual(movie);
+    expect(mockMovieService.findOne).toHaveBeenCalledWith(1);
+  })
+
+  it('should seed movies when movies are found', async () => {
+    const pages = '5';
+    mockMovieService.seedMovies.mockResolvedValue({ seeded: 100 });
+
+    const result = await controller.seedMovies({ pages });
+
+    expect(result).toEqual({ seeded: 100 });
+    expect(mockMovieService.seedMovies).toHaveBeenCalledWith(parseInt(pages));
+  })
+
+  it('should delta sync when movies are found', async () => {
+    mockMovieService.deltaSync.mockResolvedValue({ seeded: 100 });
+
+    const result = await controller.deltaSync();
+
+    expect(result).toEqual({ seeded: 100 });
+    expect(mockMovieService.deltaSync).toHaveBeenCalledTimes(1);
+  })
+
   it('should addRateToMovie when movie and user are found', async () => {
     const user = { id: 1, email: 'test@test.com' };
     const movieId = 1;
